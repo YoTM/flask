1845d0a6c1d30013d110eb236badd0acce2a0f8c
YoTM <yolkin_t@list.ru>
2018-05-23 07:38:11 +0300

    changed python interpreter to local

--- microblog/venv/bin/activate.fish
+++ microblog/venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mariache/Flask/microblog/venv"
+set -gx VIRTUAL_ENV "/home/mariache/PycharmProjects/flask/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
