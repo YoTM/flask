6ce863a1ec0fb52bb53482d447d9a8cb2c019150
tim <yolkin_t@list.ru>
2018-05-29 17:57:48 +0300

    add registration form

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -9,8 +9,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 login = LoginManager(app)
 login.login_view = 'login'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
