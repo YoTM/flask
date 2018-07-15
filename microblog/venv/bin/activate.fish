b51200d7860148e306f5de59db379f61cc5de59b
YoTM <yolkin_t@list.ru>
2018-07-15 13:04:51 +0300

    add followers, following and unittests for users

--- microblog/venv/bin/activate.fish
+++ microblog/venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/tim/Документы/flask/microblog/venv"
+set -gx VIRTUAL_ENV "/home/tim/PycharmProjects/flask/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
